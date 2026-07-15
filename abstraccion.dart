0cf7b4e5c3ef1d94d3bb62ab07c8bda3ddf2838f
0953849882-blip <0953849882@aitec.edu.ec>
2026-07-14 21:49:52 -0500

    feat: agrega clase Mago y metodo defender() para reforzar abstraccion con polimorfismo

--- abstraccion.dart
+++ abstraccion.dart
@@ -1,8 +1,29 @@
 abstract class Personaje {
   void atacar();
+  void defender(); // otro método abstracto
 }
 
 class Guerrero implements Personaje {
   @override
-  void atacar() => print('Atacar');
+  void atacar() => print('El Guerrero ataca con su espada');
+
+  @override
+  void defender() => print('El Guerrero se cubre con su escudo');
+}
+
+class Mago implements Personaje {
+  @override
+  void atacar() => print('El Mago lanza una bola de fuego');
+
+  @override
+  void defender() => print('El Mago invoca un escudo mágico');
+}
+
+void main() {
+  List<Personaje> personajes = [Guerrero(), Mago()];
+
+  for (var personaje in personajes) {
+    personaje.atacar();
+    personaje.defender();
+  }
 }
